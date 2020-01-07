1f900a11426f7ba7b169d96c2b90d000692baee3
Adstokoe19 <andrew.stokoe@surrey.ac.uk>
2020-01-07 17:21:46 +0000

    Update image example

--- play/index.js
+++ play/index.js
@@ -1,47 +1,47 @@
 /* eslint-disable */
-
+ 
 import Vue from 'vue'
 import { play } from 'vue-play'
 import Carousel3d from '../src/Carousel3d.vue'
 import Slide from '../src/Slide.vue'
-
+ 
 play('Carousel3d', module)
-
+ 
 const slides = [
     {
         title: 'Slide 1',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.',
-        src: 'https://placehold.it/360x270'
+        desc: '',
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 2',
         desc: 'Lorem ipsum dolor sit amet ',
-        src: 'https://placehold.it/360x270'
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 3',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
-        src: 'https://placehold.it/360x270'
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 4',
         desc: 'Lorem ipsum dolor sit amet,  Enim, maxime.',
-        src: 'https://placehold.it/360x270'
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 5',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.',
-        src: 'https://placehold.it/360x270'
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 6',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.',
-        src: 'https://placehold.it/360x270'
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 7',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.',
-        src: 'https://placehold.it/360x270'
+        src: 'https://picsum.photos/600/400'
     },
     {
         title: 'Slide 8',
@@ -59,7 +59,7 @@ const slides = [
         src: 'https://placehold.it/360x270'
     }
 ]
-
+ 
 play('Carousel3d', module)
     .add('default', {
         template: `<carousel-3d>
@@ -79,9 +79,11 @@ play('Carousel3d', module)
         }
     })
     .add('images', {
-        template: `<carousel-3d>
-            <slide v-for="(slide, i) in slides" :index="i">
+        template: `<carousel-3d @before-slide-change="onBeforeSlideChange" :adaptiveHeight="true" :height="height">
+            <slide v-for="(slide, i) in slides" :index="i" ref="slide">
                 <img :src="slide.src">
+                <h1>{{slide.title}}</h1>
+                <p>{{slide.desc}}</p>
             </slide>
         </carousel-3d>`,
         components: {
@@ -90,11 +92,42 @@ play('Carousel3d', module)
         },
         data () {
             return {
-                slides: slides
+                slides: slides,
+                index: 0,
+                height: 0
             }
-        }
+        },   
+        mounted() {
+            let timeout = false, // holder for timeout id
+            delay = 250; // delay after event is "complete" to run callback   
+            // window.addEventListener("resize", this.updateHeight);
+            // window.resize event listener
+            window.addEventListener('resize', () => {
+              // clear the timeout
+              clearTimeout(timeout);
+              // start timing for event "completion"
+              timeout = setTimeout(this.updateHeight, delay);
+            });
+            // allow DOM content to load
+            setTimeout(this.updateHeight, 400); 
+          },
+          destroyed() {
+            window.removeEventListener("resize", this.updateHeight);
+          },
+        methods: {
+            updateHeight() {
+                // calculate slide height and set this to the data object
+                this.height = this.$refs.slide[this.index].$el.clientHeight;
+            },
+            onBeforeSlideChange(index) {
+                // update the data object index
+                this.index = index;
+                // get height of next slide
+                this.updateHeight();
+            },
+        },
     })
-
+ 
     .add('autoplay enabled', {
         template: `<carousel-3d :autoplay="true">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -111,7 +144,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('num of displayed', {
         template: `<carousel-3d :display="3">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -128,7 +161,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('even number displayed', {
         template: `<carousel-3d :display="6" :bias="'right'">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -145,7 +178,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('slides clickable', {
         template: `<carousel-3d :clickable="true">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -162,7 +195,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('loop disabled', {
         template: `<carousel-3d :loop="false">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -179,7 +212,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('controls visible', {
         template: `<carousel-3d :controls-visible="true">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -196,9 +229,9 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('custom controls', {
-        template: `<carousel-3d :controls-visible="true" :controls-prev-html="'&#10092;'" :controls-next-html="'&#10093;'" 
+        template: `<carousel-3d :controls-visible="true" :controls-prev-html="'&#10092;'" :controls-next-html="'&#10093;'"
                                 :controls-width="30" :controls-height="60">
             <slide v-for="(slide, i) in slides" :index="i">
                 <img :src="slide.src">
@@ -214,7 +247,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('ltr direction', {
         template: `<carousel-3d :dir="'ltr'">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -231,7 +264,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('callbacks', {
         template: `<carousel-3d :on-slide-change="onSlideChanged" :on-last-slide="onLastSlide" :on-main-slide-click="onMainSlideClick">
             <slide v-for="(slide, i) in slides" :index="i" >
@@ -259,7 +292,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('add/remove slides', {
         template: `<div>
             <carousel-3d :count="slideCount">
@@ -290,7 +323,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('callbacks through emit', {
         template: `<carousel-3d
                       @before-slide-change="onSlideChange"
@@ -321,7 +354,7 @@ play('Carousel3d', module)
             }
         }
     })
-
+ 
     .add('3d Disabled', {
         template: `<carousel-3d :disable3d="true" :space="370" :clickable="false" :controls-visible="true">
             <slide v-for="(slide, i) in slides" :index="i">
@@ -355,8 +388,8 @@ play('Carousel3d', module)
                 <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
                     <div :style="{ textAlign: leftIndex >= 0 ? 'right' : 'left' }">
                         right {{ rightIndex }} <br>
-                        left {{ leftIndex }} 
-                    </div>  
+                        left {{ leftIndex }}
+                    </div> 
                     <img :data-index="index" :data-is-current="isCurrent" :src="slide.src">
                 </template>
             </slide>
@@ -371,3 +404,4 @@ play('Carousel3d', module)
             }
         }
     })
+ 
