249f07e52ec0f5b1e86e40f909da6df09dbdb79b
Liam Hall <hello@wearethreebears.co.uk>
2020-09-23 11:29:58 +0100

    Aria labels added to control links for a11y, left as links as apposed to changing to buttons to ensure styles are not effected

--- src/Controls.vue
+++ src/Controls.vue
@@ -2,12 +2,16 @@
     <div class="carousel-3d-controls">
         <a href="#" class="prev" @click.prevent="parent.goPrev()"
            :class="{ disabled: !parent.isPrevPossible }"
-           :style="`width: ${width}px; height: ${height}px; line-height: ${height}px;`">
+           :style="`width: ${width}px; height: ${height}px; line-height: ${height}px;`"
+           aria-label="Previous slide"
+        >
             <span v-html="prevHtml"></span>
         </a>
         <a href="#" class="next" @click.prevent="parent.goNext()"
            :class="{ disabled: !parent.isNextPossible }"
-           :style="`width: ${width}px; height: ${height}px; line-height: ${height}px;`">
+           :style="`width: ${width}px; height: ${height}px; line-height: ${height}px;`"
+           aria-label="Next slide"
+        >
             <span v-html="nextHtml"></span>
         </a>
     </div>
